26b8e4270db6245188efdd3049630075f270a939
Melvin Gruschow and Nicole Sanders <melvin+nicole@>
2017-03-20 15:44:45 -0700

    add delete announcement functionality

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-     return this.store.findAll('announcement');
+     return this.store.findAll('announcements');
+   },
+   actions: {
+     destroyAnnouncement(announcement) {
+       announcement.destroyRecord();
+       this.transitionTo('index');
+     }
+
    }
-  //  actions: {
-  //   saveRental3(params) {
-  //     var newRental = this.store.createRecord('rental', params);
-  //     newRental.save();
-  //     this.transitionTo('index');
-  //   }
-  // }
 });
